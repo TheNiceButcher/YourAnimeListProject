4c10dd853b9f64330cbd4626e2e37d3493bb069d
thenicebutcher <hamdaniharrisaris@gmail.com>
2022-01-28 17:52:50 -0500

    Ajout tables site

--- bdd.sql
+++ bdd.sql
@@ -6,11 +6,22 @@ CREATE TABLE Animes(
 CREATE TABLE Utilisateur (
         pseudo TEXT PRIMARY KEY,
         avatar TEXT DEFAULT '/pictures/default.jpeg',
-        mot_de_passe TEXT NOT NULL
+        mot_de_passe TEXT NOT NULL,
+        description TEXT
+);
+CREATE TABLE Animelist (
+  pseudo TEXT,
+  nanime INT,
+  PRIMARY KEY (pseudo,nanime)
 );
 INSERT INTO Animes(nom) VALUES
 ('Code Geass'),
 ('Death Note');
+INSERT INTO Utilisateur (pseudo,mot_de_passe,description) VALUES
+('Sasuke','VILLAGE','Veux mener le chaos'),
+('Kira','ryuk','Aime les pommes');
+INSERT INTO AnimeList(pseudo,nanime)
+('Kira',1);
 --DO $$
 --BEGIN
 --IF NOT EXISTS(SELECT * FROM Anime) tHEN INSERT INTO Coucou(id) VALUES ('kebab');
